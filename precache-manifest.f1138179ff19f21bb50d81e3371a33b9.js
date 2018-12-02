c62bb6702c0f877365e0c499e0e3d49c4d073ee4
Avital Ben-Natan <avital.bennatan@gmail.com>
2018-12-03 01:33:52 +0200

    deploy

---
+++ precache-manifest.f1138179ff19f21bb50d81e3371a33b9.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuetify-ts-vuexorm/robots.txt"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "0937500d37b67ef53848",
+    "url": "/vuetify-ts-vuexorm/js/about.ec1e39f7.js"
+  },
+  {
+    "revision": "46a2ab4cea69dfc7c5f6",
+    "url": "/vuetify-ts-vuexorm/js/chunk-vendors.57798efa.js"
+  },
+  {
+    "revision": "1de19fcc83a0d886b3b0",
+    "url": "/vuetify-ts-vuexorm/js/app.7aa9969f.js"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/vuetify-ts-vuexorm/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/vuetify-ts-vuexorm/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/vuetify-ts-vuexorm/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vuetify-ts-vuexorm/img/vue_logo.82b9c7a5.png"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "9a3da6c36820c7dccf2bfa7dfb943742",
+    "url": "/vuetify-ts-vuexorm/index.html"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/vuetify-ts-vuexorm/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/vuetify-ts-vuexorm/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "46a2ab4cea69dfc7c5f6",
+    "url": "/vuetify-ts-vuexorm/css/chunk-vendors.92b5162b.css"
+  }
+];
